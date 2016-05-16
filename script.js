cd6e017955dbdd974fbe25cc47fa2d8a6507a8cb
Emma Clarke <ejclarke@uw.edu>
2016-05-16 10:10:28 -0700

    Add notes

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
 'use strict'; //strict mode: catch silly errors
 
 //code goes here!
+
+console.log("Hello world!"); 
+
+var x = 4; //create and assign variable
+var y = x; //create and assign variable
+x = 5;     //assign variable
+
+console.log(x+', '+y);
+
+//create a variable (not assigned)
+var hoursSlept; 
+console.log(hoursSlept); //=> undefined
+
+//Make a new string variable for the value "The iSchool is my school"
+var motto = "The iSchool is my school";
+
+//Log out the string
+console.log(motto);
+
+//Convert the string to upper case, replacing the previous string
+//Log out the changed variable
+var motto = motto.toUpperCase();
+console.log(motto);
+
+//Use the slice method to extract characters 2 through 10 (inclusive) 
+//into a new variable, and log that variable
+var part = motto.slice(2,11);
+console.log(part);
+
+//Use the indexOf method to see if the word "cool" is in your string
+var index = motto.indexOf("cool");
+console.log(index);
